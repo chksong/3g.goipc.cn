b8c7ec4ed08728639931d9ba9a70bd008464d5be
chksong <chksong@qq.com>
2015-05-05 16:27:52 +0800

    测试 分类的输出python 列表以及字典，以及表格的使用。 和测警框的使用

--- admin/category.py
+++ admin/category.py
@@ -8,9 +8,7 @@
 
 
 from lib import common
-
-
-
+import  logging
 
 class listCategory(common.BaseHandle):
     def get(self):
@@ -19,12 +17,27 @@ class listCategory(common.BaseHandle):
             self.redirect("/")
             return
 
-        self.render("admin/category.html",admin_user=admin_user)
+        cates = [
+            {
+                "brand": "研华工控产品",
+                "cata" : ["工控机","主板", "电源"]
+            },
+            {
+                "brand": "华北工控产品",
+                "cata" : ["工控机","主板", "电源"]
+            },
+            {
+                "brand": "凌华公共产品",
+                "cata" : ["工控机","主板", "电源"]
+            },
+        ]
+
+        self.render("admin/category.html",admin_user=admin_user, brands=cates )
 
     def post(self):
         pass
 
-class BandAdd(common.BaseHandle):
+class AddBrandName(common.BaseHandle):
     def get(self):
         pass
 
@@ -32,3 +45,13 @@ class BandAdd(common.BaseHandle):
         admin_user = self.get_admin_user()
         if not admin_user:
             self.redirect("/")
+            return
+
+        brandname = self.get_argument("brandname")
+        if not brandname or len(brandname) <10 :
+            self.write({"content":"品牌字段为空或者太短", "state":-1})
+            return
+
+        logging.info("[AddBrandName] brandname=%s" % brandname )
+
+        self.write({"content":"成功", "state":1})
