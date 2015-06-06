eabf0b0a3ec5c26b6477aaca59a5604dcaf4c7e0
chksong <chksong@qq.com>
2015-06-06 12:10:15 +0800

    autoescape=None

--- goipc.py
+++ goipc.py
@@ -26,13 +26,13 @@ from lib import common, ueditorhander, upLoadFile
 
 
 
-define("port",default=8888,help="run on the given port", type=int)
-define("mysql_host", default="127.0.0.1:3306", help="blog database host")
-define("mysql_database", default="blog", help="blog database name")
-define("mysql_user", default="blog", help="blog database user")
-define("mysql_password", default="blog", help="blog database password")
-
-
+# define("port",default=8888,help="run on the given port", type=int)
+# define("mysql_host", default="127.0.0.1:3306", help="blog database host")
+# define("mysql_database", default="blog", help="blog database name")
+# define("mysql_user", default="blog", help="blog database user")
+# define("mysql_password", default="blog", help="blog database password")
+#
+#
 
 
 
@@ -81,7 +81,7 @@ class Application(tornado.web.Application):
             cookie_secret="akdhdoudisfppplkjiijjji",
             login_url="/auth/login" ,
             debug=True,
-            autoescape=None
+            autoescape=None,
         )
         tornado.web.Application.__init__(self,handlers, **setting)
         self.db = pymongo.Connection('localhost',27017).goipc
