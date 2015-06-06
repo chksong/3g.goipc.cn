7da1dee5806873ff1c3964ec5a211ecd741d8561
chksong <chksong@qq.com>
2015-06-06 13:13:40 +0800

    删除其他 define("port",default=8888,help="run on the given port", type=int)

--- goipc.py
+++ goipc.py
@@ -25,14 +25,8 @@ import admin.product as product
 from lib import common, ueditorhander, upLoadFile
 
 
+define("port",default=8888,help="run on the given port", type=int)
 
-# define("port",default=8888,help="run on the given port", type=int)
-# define("mysql_host", default="127.0.0.1:3306", help="blog database host")
-# define("mysql_database", default="blog", help="blog database name")
-# define("mysql_user", default="blog", help="blog database user")
-# define("mysql_password", default="blog", help="blog database password")
-#
-#
 
 
 
